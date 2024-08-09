ce36cb449c94fc31446828ac04e4bd7ea1c90f55
贺俊浩 <nougat_hx@qq.com>
2024-08-09 11:24:27 +0800

    init

--- src/formula/formula.service.ts
+++ src/formula/formula.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { FormulaParams, FormulaReturnType } from 'src/type';
 import { getGAnalysis } from '../lib/nearley';
+const throwErrs = [undefined, Infinity, -Infinity];
 @Injectable()
 export class FormulaService {
   getFormula(body: FormulaParams): FormulaReturnType {
     try {
       const result = getGAnalysis(body.formula)[0];
-      if (
-        !result[0] === undefined &&
-        result[0] === Infinity &&
-        result[0] === -Infinity
-      ) {
-        throw new Error('Invalid formula');
-      }
+      if (throwErrs.includes(result)) throw new Error('Invalid formula');
       return {
         success: true,
         result,
